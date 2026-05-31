060d317757e0ac9164ac99277eca52ae29a4750a
akiso <akiso-program@gmail.com>
2026-05-31 18:11:48 +0900

    for github pages

--- src/components/PageLayout.tsx
+++ src/components/PageLayout.tsx
@@ -2,9 +2,14 @@ import React from "react";
 import HeaderNav from "./HeaderNav";
 import CategorySection from "./CategorySection";
 import ItemModal from "./ItemModal";
-import items from "../data/items.json";
+import rawItems  from "../data/items.json";
 
 const PageLayout: React.FC = () => {
+  // ここで BASE_URL を付けた items に変換する
+  const items = rawItems.map(item => ({
+    ...item,
+    image: `${import.meta.env.BASE_URL}${item.image}`
+  }));
   const categories = [...new Set(items.map(item => item.category))];
   return (
     <div>
